29df506c2610c2d73c7f90ec8b739f4a8127777b
chenggang003 <1979501736@qq.com>
2018-09-10 20:33:16 +0800

    解决了一个bug

--- src/main/java/com/springboot/helloworld/HelloworldApplication.java
+++ src/main/java/com/springboot/helloworld/HelloworldApplication.java
@@ -8,6 +8,7 @@ public class HelloworldApplication {
 
     public static void main(String[] args) {
         System.out.println("你好吗");
+        System.out.println("解决了一个bug!");
         SpringApplication.run(HelloworldApplication.class, args);
     }
 }
