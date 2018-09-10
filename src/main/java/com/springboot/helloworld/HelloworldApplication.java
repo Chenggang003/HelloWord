2e6ace27f0d7013e308025ea0f3208494efd71aa
Chenggang003 <1979501736@qq.com>
2018-09-10 22:20:42 +0800

    Revert "Hotfix"

--- src/main/java/com/springboot/helloworld/HelloworldApplication.java
+++ src/main/java/com/springboot/helloworld/HelloworldApplication.java
@@ -8,7 +8,7 @@ public class HelloworldApplication {
 
     public static void main(String[] args) {
         System.out.println("你好吗");
-        System.out.println("解决了一个jbn!");
+        System.out.println("解决了一个jbm!");
         SpringApplication.run(HelloworldApplication.class, args);
     }
 }
